2d9cb6a879902e660b39ebd2a2a9d08b888a6acd
varlamovmaksim <varlamovmaksim956@gmail.com>
2026-03-10 23:06:45 +0300

    fixed root url

--- farcaster.config.ts
+++ farcaster.config.ts
@@ -1,6 +1,6 @@
-const ROOT_URL =
-  process.env.NEXT_PUBLIC_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000');
+const ROOT_URL = process.env.NEXT_PUBLIC_URL ? 'https://basoclicker.app' : 'http://localhost:3000'
+  // process.env.NEXT_PUBLIC_URL ||
+  // (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000');
 
 /**
  * MiniApp configuration object. Must follow the Farcaster MiniApp specification.
